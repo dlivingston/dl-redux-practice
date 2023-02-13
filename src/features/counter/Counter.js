bd1d10bf45fcb805df59991603a3eeec93a8a97a
Don Livingston <donlivingston@gmail.com>
2023-02-12 21:52:09 -0500

    adding sass

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment, selectCount } from "./counterSlice";
-import styles from './Counter.module.css'
+import styles from './Counter.module.scss'
 
 export function Counter() {
 	const count = useSelector(selectCount)
